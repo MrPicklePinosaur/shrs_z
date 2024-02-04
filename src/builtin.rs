fde36c3852005f412f50651eab5c1b51140843cf
Daniel Liu <mr.picklepinosaur@gmail.com>
2024-02-04 12:04:57 -0800

    frecency tracking

--- src/builtin.rs
+++ src/builtin.rs
@@ -3,21 +3,29 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
+    /// restrict matches to subdirectories of the current directory
+    #[arg(short = 'c', long)]
+    subdir: bool,
     /// echo the best match, don't cd
-    #[arg(short, long)]
+    #[arg(short = 'e', long)]
     echo: bool,
     /// list only
-    #[arg(short, long)]
+    #[arg(short = 'l', long)]
     list: bool,
     /// match by rank only
-    #[arg(short, long)]
+    #[arg(short = 'r', long)]
     rank_only: bool,
     /// match by recent access only
     #[arg(short = 't', long)]
     recent_only: bool,
+    /// remove the current directory from the datafile
+    #[arg(short = 'x', long)]
+    remove: bool,
+
+    /// Regex to match
+    regex: Vec<String>,
 }
 
-#[derive(Default)]
 pub struct ZBuiltin {
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,67 @@
 mod builtin;
 
-use shrs::prelude::*;
+use std::{collections::{HashMap, hash_map::Entry}, path::PathBuf};
+use chrono::{DateTime, Utc};
+
+use shrs::{prelude::*, anyhow::anyhow};
 use crate::builtin::ZBuiltin;
+
+struct Frecency {
+    /// Number of times the directory was accessed
+    rank: u64,
+    /// The last timestamp the directory was accessed
+    access_time: i64,
+}
+
+impl Frecency {
+    pub fn new() -> Self {
+        let now = Utc::now().timestamp();
+        Frecency { rank: 1, access_time: now }
+    }
+
+    /// Access the directory, updating it's frecency
+    pub fn access(&mut self) {
+        self.rank += 1; 
+        self.access_time = Utc::now().timestamp();
+    }
+
+    /// Return the weighted frecency value
+    pub fn value(&self) -> u64 {
+        let dx = Utc::now().timestamp() - self.access_time;
+        (10000.0 * self.rank as f64 * (3.75/((0.0001 * dx as f64 + 0.1) + 0.25))) as u64
+    }
+}
+
+#[derive(Default)]
+struct ZState {
+    /// Map of path to 'frecency'
+    database: HashMap<PathBuf, Frecency>,
+    /// Sum of all ranks in database
+    total_rank: u64
+}
+
+pub fn before_command_hook(
+    sh: &Shell,
+    sh_ctx: &mut Context,
+    sh_rt: &mut Runtime,
+    ctx: &BeforeCommandCtx,
+) -> anyhow::Result<()> {
+
+    // update access directory after each command thats ran
+    let Some(state) = sh_ctx.state.get_mut::<ZState>() else { return Err(anyhow!("could not get z state")) };
+
+    match state.database.entry(ctx.run_ctx.working_dir.clone()) {
+        Entry::Occupied(mut entry) => {
+            entry.get_mut().access();
+        },
+        Entry::Vacant(entry) => {
+            entry.insert(Frecency::new());
+        }
+    }
+    state.total_rank += 1;
+
+    Ok(())
+}
 
 pub struct ZPlugin;
 
@@ -13,7 +73,12 @@ impl ZPlugin {
 
 impl Plugin for ZPlugin {
     fn init(&self, shell: &mut ShellConfig) -> anyhow::Result<()> {
-        shell.builtins.insert("z", ZBuiltin::default());
+        let state = ZState::default();
+        shell.state.insert(state);
+
+        shell.builtins.insert("z", ZBuiltin::new());
+        shell.hooks.insert(before_command_hook);
+
         Ok(())
     }
 }
